0109136ee5044444d61812ea126abbb960225b61
snakent <tradent@mail.ru>
2020-02-16 18:09:20 +0500

    Update iblock_examples.php

--- iblock_examples.php
+++ iblock_examples.php
@@ -4,7 +4,6 @@ CModule::IncludeModule('iblock');
 /**
 * Delete all elements from sections
 */
-
 $result = CIBlockElement::GetList
 (
     array("ID"=>"ASC"),
@@ -17,3 +16,25 @@ $result = CIBlockElement::GetList
 );
 
 while($element = $result->Fetch()) CIBlockElement::Delete($element['ID']);
+
+/**
+* Resave all elements by iblock ID
+*/
+set_time_limit(0);
+CModule::IncludeModule('iblock');
+
+$result = CIBlockElement::GetList
+(
+    array("ID"=>"ASC"),
+    array
+    (
+        'IBLOCK_ID' => 4
+    )
+);
+
+$el = new CIBlockElement;
+$arFields = array(
+    "MODIFIED_BY" => $USER->GetID(),
+);
+
+while($element = $result->Fetch()) $el->Update($element['ID'], $arFields);
